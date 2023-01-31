66b92be551a7d70da851c73551f32c0189807495
tbgolfer86 <tbj86@outlook.com>
2023-01-31 13:33:14 -0500

    added license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,41 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(License) {
+  if (License == 'GNU AGPLv3') {
+    return `![GNU AGPLv3 license](https://img.shields.io/badge/license-GNU_AGPLv3-blue.svg)`;
+  }
+  if (License == 'GNU GPLv3') {
+    return `![GNU GPLv3 license](https://img.shields.io/badge/license-GNU_GPLv3-blue.svg)`;
+  }
+  if (License == 'GNU LGPLv3') {
+    return `![GNU LGPLv3 license](https://img.shields.io/badge/license-GNU_LGPLv3-blue.svg)`;
+  }
+  if (License == 'Mozilla') {
+    return `![Mozilla license](https://img.shields.io/badge/license-Mozilla-blue.svg)`;
+  }
+  if (License == 'MIT') {
+    return `![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)`;
+  }
+  if (License == 'Apache') {
+    return `![Apache license](https://img.shields.io/badge/license-Apache-blue.svg)`;
+  } 
+  if (License == 'Boost') {
+    return `![Boost license](https://img.shields.io/badge/license-Boost-blue.svg)`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(License) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(License) {}
 
 
 function generateMarkdown(data) {
   return `# ${data.Title}
-
+  ${renderLicenseBadge(data.License)}
   ## Description
   ${data.Description}
 
